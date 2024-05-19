46a401c71fabe9e6c6da28a0b4a4d37e40134cb2
Matéo Faivre <mateofaivre2@gmail.com>
2024-05-19 16:17:57 +0200

    🐛 Fix auth secret

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+	  secret: env('ADMIN_JWT_SECRET', 'dbXzYvfSS5VmgWXiHeFL1w=='),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
